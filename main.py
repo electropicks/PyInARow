cf505b217e54b576909825ca843a5700048f291e
electropicks <lkbidy@calpoly.edu>
2022-08-20 17:01:16 +0200

    refactored main file to use main

---
+++ main.py
@@ -0,0 +1,24 @@
+from utils import *
+
+if __name__ == "__main__":
+    print("""Hello! Welcome to Py in a Row, the game where you have to align about 3.14 pieces of your color before your opponent does so with theirs.""")
+    print("""
+    ██████╗░██╗░░░██╗██╗███╗░░██╗░█████╗░██████╗░░█████╗░░██╗░░░░░░░██╗
+    ██╔══██╗╚██╗░██╔╝██║████╗░██║██╔══██╗██╔══██╗██╔══██╗░██║░░██╗░░██║
+    ██████╔╝░╚████╔╝░██║██╔██╗██║███████║██████╔╝██║░░██║░╚██╗████╗██╔╝
+    ██╔═══╝░░░╚██╔╝░░██║██║╚████║██╔══██║██╔══██╗██║░░██║░░████╔═████║░
+    ██║░░░░░░░░██║░░░██║██║░╚███║██║░░██║██║░░██║╚█████╔╝░░╚██╔╝░╚██╔╝░
+    ╚═╝░░░░░░░░╚═╝░░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░░░░╚═╝░░░╚═╝░░""")
+    #setup done
+
+    #game begins
+    p1 = input("Player 1, input your name: ").title()
+    p1 = p1[:min(len(p1), 10)]
+    p2 = input("Player 2, input your name: ").title()
+    p2 = p2[:min(len(p2), 10)]
+
+
+    game = Game(p1, p2, rows=6, cols=7)
+    for i in range(10):
+        game.play()
+
